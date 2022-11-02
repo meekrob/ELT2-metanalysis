1a34914b6ff13c9457a8015a947ae33888cc6f60
David <dcking@colostate.edu>
2022-11-02 14:07:26 -0600

    organizing intersections by stage

--- summit/modENCODEtracks/intersection_LE.sh
+++ summit/modENCODEtracks/intersection_LE.sh
@@ -2,7 +2,10 @@
 set -e # quit on error
 for fname in data/orig/*_LE.*
 do
+       # add intersection to output name
        outfile=${fname/.bed/.intersection.bed}
+       # replace orig/ with intersection/LE
+       outfile=${outfile/orig/intersections\/LE}
        bedtools intersect  -c -f 0.08333333 -a script_data/intestine_enriched_by_stage/promoters_embryo_bound_TrueFalse.bed -b $fname > $outfile
        echo "wrote $outfile"
 done
