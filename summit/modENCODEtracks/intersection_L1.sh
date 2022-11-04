6a810a133ab9d0ebe45d0d9422b594b0ff9c28eb
David <dcking@colostate.edu>
2022-11-04 10:32:17 -0600

    Changed promoter input file for shell script
    changed promoter input for shell script

--- summit/modENCODEtracks/intersection_L1.sh
+++ summit/modENCODEtracks/intersection_L1.sh
@@ -4,6 +4,6 @@ for fname in data/orig/*_L1.*
 do
     outfile=${fname/.bed/.intersection.bed}
     outfile=${outfile/orig/intersections\/L1}
-    bedtools intersect -c -f 0.0833333 -a script_data/intestine_enriched_by_stage/promoters_embryo_bound_TrueFalse.bed -b $fname > $outfile
+    bedtools intersect -c -f 0.0833333 -a script_data/intestine_enriched_by_stage/promoters_L1_bound_TrueFalse.bed -b $fname > $outfile
     echo "wrote $outfile"
 done
