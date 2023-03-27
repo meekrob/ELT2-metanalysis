85e75bb9da66aa3c376718c9db2abe2feda28cb4
David <davidcking.inbox@gmail.com>
2023-03-27 14:44:43 -0600

    updated DBPeakOverlaps.R

--- DBPeakOverlaps.R
+++ DBPeakOverlaps.R
@@ -3,22 +3,47 @@ library(tidyverse)
 library(dplyr)
 library(magrittr)
 library(RMariaDB)
+source('DB_connect.R')
 
-onishDATA <- dbConnect(
-  drv = RMariaDB::MariaDB(), 
-  username = "worm",
-  host = "129.82.125.11", 
-  port = 3307, dbname = "NishimuraLab"
-)
-AllPromoter_binding = dbReadTable(onishDATA, "AllPromoter_binding")
-dbDisconnect(onishDATA)
+# load saved data
+key = list("DBPeakOverlaps")
+DBPeakOverlaps <- loadCache(key)
+if (!is.null(DBPeakOverlaps)) {
+  cat("Loading DBPeakOverlaps from cache\n")
+} else
+{ # recalculate
+  
+  # load saved table if available
+  cat("DBPeakOverlaps not cached... Checking for cached table from DB.")
+  key = list("AllPromoter_binding")
+  AllPromoter_binding <- loadCache(key)
+  if (is.null(AllPromoter_binding)){
+    cat("DB table not cached. Connecting to database.")
+    onishDATA = onishDBConnect()
+    AllPromoter_binding = dbReadTableCached(onishDATA, "AllPromoter_binding")
+    dbDisconnect(onishDATA)
+  }
+  cat("calculating overlaps...")
+  # aggregate overlaps by gene, ChIP-seq experiment
+  overlapping = AllPromoter_binding %>% group_by(name, tfStage) %>% summarize(n=n()) 
+  # check pha_4 counts
+  overlapping %>% filter(tfStage == "pha-4_LE_1")
+  overlapping %>% filter(tfStage == "pha-4_L3_1") %>% nrow()
+  # pivot by gene name
+  DBPeakOverlaps = pivot_wider(overlapping, id_cols = name, names_from = tfStage, values_from = n, values_fill = 0)
+  
+  key = list("DBPeakOverlaps")
+  saveCache(DBPeakOverlaps, key=key)
+}
 
-# aggregate overlaps by gene, ChIP-seq experiment
-AllPromoter_binding %>% group_by(name, tfStage) %>% summarize(n=n()) -> overlapping
-overlapping %>% filter(tfStage == "pha-4_LE_1")
-overlapping %>% filter(tfStage == "pha-4_L3_1") %>% nrow()
+DBPeakOverlaps[1:10,1:10]
 
-# pivot by gene name
-tbl = pivot_wider(overlapping, id_cols = name, names_from = tfStage, values_from = n, values_fill = 0)
-tbl[1:10,1:10]
+
+
+
+
+
+
+
+
 
