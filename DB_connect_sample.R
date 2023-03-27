dc9ab9c2fceaa21d0452fd5dd2b30038f30cde91
David <davidcking.inbox@gmail.com>
2023-03-27 14:46:32 -0600

    Updated DB_connect_sample.R

--- DB_connect_sample.R
+++ DB_connect_sample.R
@@ -3,6 +3,9 @@ ONISHDB_USERNAME=""
 ONISHDB_HOST=""
 ###
 
+library(dplyr)
+
+# less commonly installed?
 if (! require(R.cache)) {
   install.packages('R.cache')
   library(R.cache)
@@ -12,17 +15,40 @@ if (! require(RMariaDB)) {
   library(RMariaDB)
 }
 
-onishDBConnect<- function() {
+onishDBListDBs <- function(onishDATA) {
+  dbIDs = dbListObjects(onishDATA) %>% filter(is_prefix == TRUE) %>% pull(name)
+  dbNames = unlist(lapply(dbIDs, function(x) x@name))
+  names(dbNames) <- NULL # they are all "schema"
+  return(dbNames)
+}
+
+onishDBConnect<- function(dbName = "NishimuraLab") {
+  cat("connecting to database", dbName, "...")
+  canConnect <-  dbCanConnect(
+    drv = RMariaDB::MariaDB(), 
+    username = ONISHDB_USERNAME,
+    host = ONISHDB_HOST, 
+    port = 3307, dbName = dbName,
+    timeout = 30
+  )  
+  if (!canConnect) {
+    cat("Error attempting to connect to", ONISHDB_HOST, "\n")
+    cat("Are you on the VPN?")
+    return(NULL)
+  }
   onishDATA <- dbConnect(
     drv = RMariaDB::MariaDB(), 
     username = ONISHDB_USERNAME,
     host = ONISHDB_HOST, 
-    port = 3307#, dbname = "NishimuraLab"
+    port = 3307, dbName = dbName,
+    timeout = 30
   )  
+  cat("done.", append = TRUE)
   return(onishDATA)
 }
 
-dbReadTableCached = function(DBConnection, tableName, force=FALSE, ...) {
+dbReadTableCached = function(tableName, ...) {
+  
   key = list(tableName)
   data <- loadCache(key)
   if (!is.null(data)) {
@@ -30,6 +56,7 @@ dbReadTableCached = function(DBConnection, tableName, force=FALSE, ...) {
     return(data);
   }
   cat("Not cached... Loading from database.")
+  DBConnection = onishDBConnect()
   data=dbReadTable(DBConnection, tableName) 
   saveCache(data, key=key)
   return(data)
